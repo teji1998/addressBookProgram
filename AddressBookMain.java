f544aa4a68e4234dba600add2559559244b8bd71
“teji1998” <tejasvinirpk@gmail.com>
2020-08-12 22:15:46 +0530

    Added:Main Method for AddressBook.

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,6 +3,8 @@ package com.addressbook;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program.");
+		AddressBook address=new AddressBook();
+		address.menu();
 	}
 }
 
