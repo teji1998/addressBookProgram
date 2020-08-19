c1e98eaee200b1b4a30d5b87b777ae552a1fc8f0
“teji1998” <tejasvinirpk@gmail.com>
2020-08-19 18:35:04 +0530

    Refactored:Corrected the indentations.

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,7 +3,7 @@ package com.addressbook;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program.");
-		AddressBook address=new AddressBook();
+		AddressBook address = new AddressBook();
 		address.menu();
 	}
 
